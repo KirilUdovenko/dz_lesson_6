fb289c15c6d52eb35bfe369955a2b05c9440003c
Kereeshkaa <kiril.suff1@gmail.com>
2021-11-04 20:29:44 +0200

    is not finished

---
+++ dz_lesson_6.py
@@ -0,0 +1,25 @@
+# def create_domains(domains):
+#     list_str = domain_name
+#     return list_str
+#
+# with open("domains.txt", "r") as txt_file:
+#     domains = txt_file.read()
+#     domain_name = domains.replace(".", '')
+# list_str = create_domains(domains)
+# print(list_str)
+#
+
+##############
+
+# def name_file(names):
+
+
+
+with open("names.txt", "r") as txt_file:
+    name = txt_file.readlines()
+
+print(name)
+
+
+# with open("names.txt", "r") as txt_file:
+#     name = txt_file.readlines()
