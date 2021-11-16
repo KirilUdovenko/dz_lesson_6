6b8fbb2748a5fe3711ea9bff5c053bf5a4213e01
Kereeshkaa <kiril.suff1@gmail.com>
2021-11-16 16:30:14 +0200

    finished

--- dz_lesson_6.py
+++ dz_lesson_6.py
@@ -8,22 +8,59 @@
 # list_str = create_domains(domains)
 # print(list_str)
 #
+#
 
-##############
-import re
-# def name_file(names):
-
-
-
-with open("names.txt", "r") as txt_file:
-    name = txt_file.read()
-    str_1 = ''.join(name)
-    # numbers = re.findall(r"[0-9]+", str_1)
-    str_2 = re.sub(r'[^\w\s]+|[\d]+', r'', str_1).strip().split("\n")
-    print(str_2)
-with open("names_2.txt", "w") as txt_file:
-    txt_file.writelines(str_2)
-with open("names_2.txt", "r") as txt_file:
-    second_name = txt_file.readlines()
+def read_file(filename):
+    with open(filename, 'r') as file:
+        return file.read().split('\n')
 
 
+def get_domains(filename):
+    lines = read_file(filename)
+    lines = [line.replace(".", "") for line in lines]
+    return lines
+
+
+def get_names(filename):
+    lines = read_file(filename)
+    lines = [line.split("\t")[1] for line in lines]
+    return lines
+
+
+def str_to_date(some_str):
+    months_dict = {'January': '01',
+                   'February': '02',
+                   'March': '03',
+                   'April': '04',
+                   'May': '05',
+                   'June': '06',
+                   'July': '07',
+                   'August': '08',
+                   'September': '09',
+                   'October': '10',
+                   'November': '11',
+                   'December': '12'}
+    parts = some_str.split()
+    day, month, year = parts
+    date = f"{day[:-2].zfill(2)}/{months_dict[month]}/{year}"
+    return date
+
+
+def get_dates(filename):
+    lines = read_file(filename)
+    lines = [line.split(" - ")[0] for line in lines if " - " in line]
+    lines = [str_to_date(line) for line in lines]
+    return lines
+
+
+filename_domains = "domains.txt"
+domains = get_domains(filename_domains)
+print(domains)
+
+filename_names = "names.txt"
+names = get_names(filename_names)
+print(names)
+
+filename_dates = "author.txt"
+dates = get_dates(filename_dates)
+print(dates)
